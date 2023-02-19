383c8bf6555bbc228afe9502e7ede21f8565f542
Eric Vallee <eric_vallee@webificservices.com>
2023-02-19 15:02:21 -0500

    feat: INFRA-273 Progress implementing templating. Updated usage example config with capitalization

--- template/template.go
+++ template/template.go
@@ -1,8 +1,17 @@
 package template
 
 import (
+	"io/ioutil"
+	"path"
+	"text/template"
 	yaml "gopkg.in/yaml.v2"
 )
+
+type TemplateParameters struct {
+	Repo    string
+	Service string
+	Release string
+}
 
 type FsConventions struct {
 	Naming    string
@@ -16,18 +25,61 @@ type Orchestration struct {
 	AppFiles      []string
 }
 
-func loadFsConventions(path string) (*FsConventions, error) {
+func loadFsConventions(path string, params *TemplateParameters) (*FsConventions, error) {
+	var conv FsConventions
+	
+	f, err := ioutil.ReadFile(path.Join(path, "filesystem-conventions.yml"))
+	if err != nil {
+		return nil, err
+	}
+
+	tmpl, tErr := template.New("FsConventions").Parse(f)
+	if tErr != nil {
+		return nil, tErr
+	}
+
+	var b bytes.Buffer
+	exErr = tmpl.Execute(&b, params)
+	if exErr != nil {
+		return nil, exErr
+	}
+
+	yamlErr = yaml.Unmarshal(b, &c)
+	if yamlErr != nil {
+		return nil, yamlErr
+	}
+
+	return FsConventions, nil
+}
+
+func loadFluxcdFile(path string, params *TemplateParameters) (string, error) {
+	//path.Join(path, "fluxcd.yml")
 	return nil, nil
 }
 
-func loadFluxcdFile(path string) (string, error) {
+func loadAppFiles(path string, params *TemplateParameters) ([]string, error) {
+	//path.Join(path, "app")
 	return nil, nil
 }
 
-func loadAppFiles(path string) ([]string, error) {
-	return nil, nil
-}
+func LoadTemplate(path string, params *TemplateParameters) (*Orchestration, error) {
+	var o Orchestration
 
-func LoadTemplate(path string) (*Orchestration, error) {
-	return nil, nil
+	o.FsConvention, fsCoErr := loadFsConventions(path, params)
+	if fsCoErr != nil {
+		return nil, fsCoErr
+	}
+
+	o.FluxcdFile, flFiErr := loadFluxcdFile(path, params)
+	if flFiErr != nil {
+		return nil, flFiErr
+	}
+
+
+	o.AppFiles, appFlsErr := loadAppFiles(path, params)
+	if appFlsErr != nil {
+		return nil, appFlsErr
+	}
+
+	return o, nil
 }
